304a0b6a5736c66c490288b13fa743383ccd5770
abhra bhattacharyya <bhattacharyya.abhra@gmail.com>
2018-12-02 23:36:30 +0530

    New Component dashboard and transactions are added with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'transactions', component: TransactionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
